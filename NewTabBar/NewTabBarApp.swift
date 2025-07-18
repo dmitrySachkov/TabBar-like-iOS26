a8618890d28e48328dbc71b665c2cca6d472e31d
domont <d.a.sachkov@gmail.com>
2025-07-18 10:21:18 +0300

    Initial Commit

---
+++ NewTabBar/NewTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTabBarApp.swift
+//  NewTabBar
+//
+//  Created by Dmitry Sachkov on 18.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
